ca3b967e8a5001fdff0d5137ef184e188246535e
ganeshkalyan7 <ganeshkalyan506@gmail.com>
2022-08-17 21:31:38 +0530

    new commit

--- routers/SeedRoutes.js
+++ routers/SeedRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const seedRouter = express.Router();
-const Product = require(".../models/productModel");
+const Product = require("../models/ProductModel");
 const User = require("../models/userModel");
 const data = require("../Data");
 
